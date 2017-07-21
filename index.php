2fc1b98dd3224e3d9a145d3ea5b0a8507b4b22e3
selma <selma@gmail.com>
2017-07-21 14:26:14 +0200

    Les preparations des requetes pour  executer

--- index.php
+++ index.php
@@ -7,32 +7,37 @@ try {
 	    $stmt = $pdo->query('SELECT * FROM `group`');
 	    // On récupére directement tous les groupes.
 	    $groups = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-}catch (Exeption $e) {
-    echo 'fail to contact DB: ' .$e->getMessage();
-    exit(1);
-}
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Music DB</title>
-</head>
-<body>
-    <h1>My Music DB</h1>
-
-    <!-- Cree une liste d'article à partir de $groups -->
-    <?php foreach ($groups as $g) { ?>
-    <article>
-       <h2><?php echo $g['name']; ?></h2> 
-       <ul>
-           <li><?php echo $g['start']; ?></li>
-           <li><?php echo $g['end']; ?></li>
-           <li><?php echo $g['origin']; ?></li>
-       </ul>
-    </article>
-    <?php } ?>
-</body>
-</html>
+	} catch (Exception $e) {
+	    header('Content-Type: text/plain');
+	    echo 'fail to contact DB: ' . $e->getMessage();
+	    exit(1);
+	}
+	?><!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	    <title>Music DB</title>
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body class="container">
+	    <div class="page-header"><h1 class="text-center">Music DB</h1></div>
+	    <ul class="nav nav-pills">
+	        <li role="presentation" class="active"><a href="index.php">Home</a></li>
+	        <li role="presentation"><a href="example/new_group.php">New Group</a></li>
+	    </ul>
+	    <!-- On crée une liste d'article à partir de $groups -->
+	    <?php foreach ($groups as $g) { ?>
+	    <article class="col-sm-offset-2 col-sm-8">
+	    <h2><a href="group.php?<?php echo 'id='.$g['id']. '&
+                    name=' .$g['name'];?>"><?php echo $g['name']; ?></a></h2>
+	        <ul>
+	            <li><?php echo $g['start'];?></li>
+	            <li><?php echo $g['end'];?></li>
+	            <li><?php echo $g['origin'];?></li>
+	        </ul>
+	    </article>
+	    <?php } ?>
+	</body>
+	</html>
